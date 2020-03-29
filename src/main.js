40067e0363a97ef87c804c8d6c348c647c9e16d5
MY <715711877@qq.com>
2020-03-29 10:22:32 +0800

    增加进度条

--- src/main.js
+++ src/main.js
@@ -12,12 +12,21 @@ import 'quill/dist/quill.core.css' // import styles
 import 'quill/dist/quill.snow.css' // for snow theme
 import 'quill/dist/quill.bubble.css' // for bubble theme
 
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 Vue.use(VueQuillEditor)
 
 Vue.prototype.$http = axios
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// request拦截器中 展示进度条
 axios.interceptors.request.use(config => {
+  NProgress.start()
   config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+// response拦截器，隐藏进度条
+axios.interceptors.response.use(config => {
+  NProgress.done()
   return config
 })
 Vue.config.productionTip = false
